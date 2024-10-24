ee97383c2020e9cecef89895142a7c18c3049104
Tomo2433 <tomaszlech56@gmail.com>
2024-10-24 16:43:02 +0200

    Refactor controllers and add Deck and Flashcard APIs
    Removed WeatherForecastController and WeatherForecast classes.
    Added DeckController with CRUD endpoints.
    Added FlashcardController with CRUD endpoints.

---
+++ Controllers/DeckController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FishkiAC.Controllers;
+
+[ApiController]
+[Route("/api/v1/[controller]")]
+public class DeckController : ControllerBase
+{
+    public DeckController()
+    {
+    }
+
+    [HttpGet]
+    public IActionResult GetById(Guid Id)
+    {
+        return Ok($"Hello, World! id: {Id}");
+    }
+
+    [HttpGet]
+    public IEnumerable<string> GetById()
+    {
+        return new List<string> { "value1", "value2" };
+    }
+
+    [HttpPost]
+    public IActionResult Post([FromBody] string value)
+    {
+        return Ok($"Hello, World! value: {value}");
+    }
+
+    [HttpPut]
+    public IActionResult Put(Guid Id, [FromBody] string value)
+    {
+        return Ok($"Hello, World! id: {Id}, value: {value}");
+    }
+
+    [HttpDelete]
+    public IActionResult Delete(Guid Id)
+    {
+        return Ok($"Hello, World! id: {Id}");
+    }
+}
