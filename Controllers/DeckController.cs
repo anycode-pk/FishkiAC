255148e2e61e3a373e8f2919c6aa1f86b5217c32
Tomo2433 <tomaszlech56@gmail.com>
2024-10-24 16:53:09 +0200

    Update routes and method names in controllers
    - Updated route attributes for `DeckController` and `FlashcardController` to use specific names.
    - Modified `GetById` method in both controllers to accept a `Guid` parameter in the route.
    - Renamed `GetById` method to `GetAll` in both controllers to reflect its functionality of returning a list of strings.

--- Controllers/DeckController.cs
+++ Controllers/DeckController.cs
@@ -3,21 +3,21 @@
 namespace FishkiAC.Controllers;
 
 [ApiController]
-[Route("/api/v1/[controller]")]
+[Route("/api/v1/Deck")]
 public class DeckController : ControllerBase
 {
     public DeckController()
     {
     }
 
-    [HttpGet]
+    [HttpGet("{Id}")]
     public IActionResult GetById(Guid Id)
     {
         return Ok($"Hello, World! id: {Id}");
     }
 
     [HttpGet]
-    public IEnumerable<string> GetById()
+    public IEnumerable<string> GetAll()
     {
         return new List<string> { "value1", "value2" };
     }
--- Controllers/FlashcardController.cs
+++ Controllers/FlashcardController.cs
@@ -3,21 +3,21 @@
 namespace FishkiAC.Controllers;
 
 [ApiController]
-[Route("/api/v1/[controller]")]
+[Route("/api/v1/Flashcard")]
 public class FlashcardController : ControllerBase
 {
     public FlashcardController()
     {
     }
 
-    [HttpGet]
+    [HttpGet("{Id}")]
     public IActionResult GetById(Guid Id)
     {
         return Ok($"Hello, World! id: {Id}");
     }
 
     [HttpGet]
-    public IEnumerable<string> GetById()
+    public IEnumerable<string> GetAll()
     {
         return new List<string> { "value1", "value2" };
     }
